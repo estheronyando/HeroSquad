13b004029bd75226fb10772c51695c8ef3daacfd
Esther <eonyando89@gmail.com>
2023-11-13 10:21:30 +0300

    Updating Squad Data Access Object inteface methods

--- src/main/java/org/example/dao/SquadDao.java
+++ src/main/java/org/example/dao/SquadDao.java
@@ -1,4 +1,29 @@
 package org.example.dao;
 
+import org.example.models.Hero;
+import org.example.models.Squad;
+
+import java.util.List;
+
 public interface SquadDao {
+
+    List<Squad> getAll();
+
+
+    void add(Squad task);
+
+
+    Squad findById(int id);
+
+
+    void update(int id, String name, int size, String cause);
+
+
+    void deleteById(int id);
+
+    void clearAllSquads();
+
+    List<Hero> getAllHeroesBySquad(int squadId);
+
+    void deleteAllHeroesInSquad(int squadId);
 }
