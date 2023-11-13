ae9c31304fe4d4c17704fd7e16185f5411780520
Esther <eonyando89@gmail.com>
2023-11-13 10:13:35 +0300

    Adding HeroDao Inteface class

--- src/main/java/org/example/dao/HeroDao.java
+++ src/main/java/org/example/dao/HeroDao.java
@@ -1,4 +1,26 @@
 package org.example.dao;
 
-public class HeroDao {
+import org.example.models.Hero;
+
+import java.util.List;
+
+public interface HeroDao {
+
+    List<Hero> getAll();
+
+
+    void add(Hero task);
+
+
+    Hero findById(int id);
+
+
+    void update(int id, String name, int age, String power, String cause, int squadId);
+
+
+    void deleteById(int id);
+
+    void clearAllHeroes();
+
+    void uploadImage(int id, byte[] image);
 }
