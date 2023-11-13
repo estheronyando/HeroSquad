0d9d605e28dcb131dc1b660a54ddf2c67296eed5
Esther <eonyando89@gmail.com>
2023-11-13 10:22:43 +0300

    Creating squad class and its methods

--- src/main/java/org/example/models/Squad.java
+++ src/main/java/org/example/models/Squad.java
@@ -1,4 +1,65 @@
 package org.example.models;
 
+import java.util.Objects;
+
 public class Squad {
+    private String name;
+    private String cause;
+    private int size;
+    private int id;
+
+    public Squad(String name, int size, String cause) {
+        this.name = name;
+        this.size = size;
+        this.cause = cause;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSquad_name(String name) {
+        this.name = name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Squad squad = (Squad) o;
+        return getSize() == squad.getSize() &&
+                getName().equals(squad.getName()) &&
+                getCause().equals(squad.getCause());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getCause(), getSize());
+    }
 }
