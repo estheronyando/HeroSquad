381d3faadff5699c1c0b81cc12e1f5d3fcd77afe
Esther <eonyando89@gmail.com>
2023-11-13 09:59:29 +0300

    Initial Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import java.util.ArrayList;
+import java.util.List;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import javax.sound.sampled.Port;
+
+import static spark.Spark.*;
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
