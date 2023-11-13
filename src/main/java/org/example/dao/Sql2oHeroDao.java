ae261b618379c8a5c2ede3b16aa8ea01571962e0
Esther <eonyando89@gmail.com>
2023-11-13 10:16:40 +0300

    Adding sql2oherodao class

--- src/main/java/org/example/dao/Sql2oHeroDao.java
+++ src/main/java/org/example/dao/Sql2oHeroDao.java
@@ -1,4 +1,98 @@
 package org.example.dao;
 
+import org.example.models.Hero;
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
 public class Sql2oHeroDao {
+    @Override
+    public void add(Hero hero) {
+        String sql = "INSERT INTO  heroes (name,age,power,weakness,squadId,image) VALUES (:name,:age,:power,:weakness,:squadId,:image)";
+        try(Connection con = DB.sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(hero)
+                    .executeUpdate()
+                    .getKey();
+            hero.setId(id);
+            System.out.println(hero.getId());
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<Hero> getAll() {
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery("SELECT * FROM heroes")
+                    .executeAndFetch(Hero.class);
+        }
+    }
+
+    @Override
+    public Hero findById(int id) {
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery("SELECT * FROM heroes WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Hero.class);
+        }
+    }
+
+
+    @Override
+    public void update(int id, String newName, int newAge, String newPower, String newWeakness, int newSquadId){
+        String sql = "UPDATE heroes SET name = :name, age = :age, power = :power, weakness = :weakness, squadId = :squadId WHERE id=:id";
+        try(Connection con = DB.sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("name", newName)
+                    .addParameter("id", id)
+                    .addParameter("age", newAge)
+                    .addParameter("power", newPower)
+                    .addParameter("weakness", newWeakness)
+                    .addParameter("squadId", newSquadId)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from heroes WHERE id=:id";
+        try (Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void clearAllHeroes() {
+        String sql = "DELETE from heroes";
+        try (Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void uploadImage(int id, byte[] image) {
+        String sql = "UPDATE heroes SET image = :image WHERE id=:id";
+        try(Connection con = DB.sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .addParameter("image", image)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
+
 }
